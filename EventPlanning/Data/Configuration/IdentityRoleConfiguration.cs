8f7dc16b7f02bc8b08e2bcb93890b8c6676a04ff
Paul <pavel.k.313@mail.ru>
2024-08-18 21:11:54 +0300

    Added IdentityRole configuration

---
+++ EventPlanning/Data/Configuration/IdentityRoleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventPlanning.Data.Configuration
+{
+    public class IdentityRoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Name = "Guest", NormalizedName = "Guest" }
+            );
+        }
+    }
+}
