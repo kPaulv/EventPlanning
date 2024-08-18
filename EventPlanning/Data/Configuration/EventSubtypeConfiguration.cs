0dd9aef0065eb52ab1498851a8dc512110a60e97
Paul <pavel.k.313@mail.ru>
2024-08-18 21:02:10 +0300

    Added entities config .cs for seeding entities

---
+++ EventPlanning/Data/Configuration/EventSubtypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EventPlanning.Data.Entities;
+
+namespace EventPlanning.Data.Configuration
+{
+    public class EventSubtypeConfiguration
+    {
+        public void Configure(EntityTypeBuilder<EventSubtype> builder)
+        {
+            builder.HasData(
+                new EventSubtype
+                {
+                    Id = 1,
+                    Name = "Rock",
+                    TypeId = 2
+                },
+                new EventSubtype
+                {
+                    Id = 2,
+                    Name = "Classical music",
+                    TypeId = 2
+                }
+            );
+        }
+
+    }
+}
---
+++ EventPlanning/Data/Configuration/EventTypeConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using EventPlanning.Data.Entities;
+
+namespace EventPlanning.Data.Configuration
+{
+    public class EventConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.HasData(
+                new Event
+                {
+                    Id = 1,
+                    Name = "Football Match",
+                    TypeId = 1
+                },
+                new Event
+                {
+                    Id = 2,
+                    Name = "Test Event",
+                    TypeId = 2
+                }
+            );
+        }
+
+    }
+}
