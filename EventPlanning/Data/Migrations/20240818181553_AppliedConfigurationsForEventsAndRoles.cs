9639109c6a179d9f77fe4f431763d8aae1ce08db
Paul <pavel.k.313@mail.ru>
2024-08-18 21:16:48 +0300

    Created Migrations for Applying configurations and seeding DB

---
+++ EventPlanning/Data/Migrations/20240818181553_AppliedConfigurationsForEventsAndRoles.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventPlanning.Data.Migrations
+{
+    public partial class AppliedConfigurationsForEventsAndRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1a13f68d-a1d0-4d23-869b-458c301ca761", "5594ae47-6482-4b0d-9a8c-36822ebf1753", "Admin", "ADMIN" },
+                    { "ca9e45d0-785b-408b-a5df-568d4c075e6f", "030f96b3-341f-464e-b064-8f234e461ea1", "Guest", "Guest" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "EventTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Sport" },
+                    { 2, "Music" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Events",
+                columns: new[] { "Id", "Name", "TypeId" },
+                values: new object[,]
+                {
+                    { 1, "Football Match", 1 },
+                    { 2, "Test Event", 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "eventSubtypes",
+                columns: new[] { "Id", "Name", "TypeId" },
+                values: new object[,]
+                {
+                    { 1, "Rock", 2 },
+                    { 2, "Classical music", 2 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1a13f68d-a1d0-4d23-869b-458c301ca761");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ca9e45d0-785b-408b-a5df-568d4c075e6f");
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Events",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "eventSubtypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "eventSubtypes",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "EventTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "EventTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
