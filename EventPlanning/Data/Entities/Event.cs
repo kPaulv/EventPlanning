5cdb339c214984e61c47687c5429ffc645cd9bd0
Paul <pavel.k.313@mail.ru>
2024-08-20 14:02:25 +0300

    New migration for new columns in Event

--- EventPlanning/Data/Entities/Event.cs
+++ EventPlanning/Data/Entities/Event.cs
@@ -7,6 +7,9 @@ namespace EventPlanning.Data.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public int TypeId { get; set; }
-        public EventType Type { get; set; }
+        public EventType? Type { get; set; }
+        public string? Dresscode { get; set; }
+        public string? Location { get; set; }
+        public string? Participants { get; set; }
     }
 }
